011a4eaf473bca1e570911a6d2ded9639d945cae
Ava <chihchi@student.unimelb.edu.au>
2020-12-12 22:41:33 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import RecipeInput from './RecipeInput';
+import RecipeList from './RecipeList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "spaghetti.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "milkshake.jpg"
+        },
+        {
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "avocado_toast.jpg"
+        }
+      ],
+      nextRecipeId: 3,
+      showForm: false,
+    }
+    this.handleSave = this.handleSave.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleSave(recipe) {
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      return {
+        recipes: [...this.state.recipes, newRecipe],
+        nextRecipeId: prevState.nextRecipeId + 1,
+        showForm: false,
+      }
+    });
+  }
+
+  handleDelete(id) {
+    const recipes = this.state.recipes.filter(r => r.id !== id);
+    this.setState({recipes});
+  }
+
+  render() {
+    const { showForm } = this.state;
+    return (
+      <div className="App">
+        <Navbar onNewRecipe={() => this.setState({ showForm: true })}/>
+        { showForm 
+          ? <RecipeInput 
+              onSave={this.handleSave} 
+              onClose={() => this.setState({ showForm: false })}
+            />
+          : null
+        } 
+        <RecipeList recipes={this.state.recipes} onDelete={this.handleDelete}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
+
